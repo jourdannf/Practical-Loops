455a12872602a48679543af0a90ef7d0c153d223
jourdannf <jourdann.fraser@gmail.com>
2024-05-23 14:42:16 -0400

    Finished Fizz Buzz

--- practical_loops.js
+++ practical_loops.js
@@ -1,1 +1,24 @@
 // Fizz Buzz
+//Loop throught numbers from 1 to 100
+///If a num is divisible by 3 -> Fizz
+//divisible by 5 -> Buzz
+//divsiible by 3 and 5 -> Fizz Buzz
+//Neither -> log the number
+
+for (let i = 0; i <= 100; i++){
+    let phrase = "";
+    if (i%3 !== 0 & i%5 !== 0){
+        console.log(i);
+    }else {
+        if (i%3 === 0){
+            phrase = "Fizz";
+        }
+        if (i%5 === 0 && phrase != ""){
+            phrase = phrase + " Buzz";
+        }else {
+            phrase = "Buzz";
+        }
+        console.log(phrase);
+    }
+    
+}
